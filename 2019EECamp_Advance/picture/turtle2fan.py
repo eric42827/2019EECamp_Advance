5f17aeab9f7637b7c9908871b27ace79f3d8ad41
whoami90506 <whoami90506@github.com>
2019-07-17 20:58:53 +0800

    add turtle2fan

---
+++ 2019EECamp_Advance/picture/turtle2fan.py
@@ -0,0 +1,16 @@
+import stripe
+import turtle as tt
+
+OUTPUT_FILE = 'picture.h'
+
+############################
+# draw your picture below  #
+############################
+
+
+############################
+# draw your picture above  #
+############################
+
+stripe.turtle2lines(OUTPUT_FILE, 64)
+tt.down()
