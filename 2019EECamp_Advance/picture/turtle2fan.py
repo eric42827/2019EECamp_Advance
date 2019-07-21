abdff08a60158b6f8f75f4f69cae70cb7b2f1e51
whoami90506 <whoami90506@gmail.com>
2019-07-22 00:37:21 +0800

    add flat rm white

--- 2019EECamp_Advance/picture/turtle2fan.py
+++ 2019EECamp_Advance/picture/turtle2fan.py
@@ -7,11 +7,21 @@ from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
 #import cairosvg
 
+############################
+# edit the setting below   #
+############################
+
 OUT_FILE = 'picture.h' # modify this line
+FLATTEN  = False # modify this line
+
+############################
+# edit the setting above   #
+############################
 
 parser = ArgumentParser(description='A program that convert the picture to LED stripes.')
 parser.add_argument('-o', dest="out", default=OUT_FILE, help='output header name.')
 parser.add_argument('-n', dest="num", type=int, default=64, help='number of stripes.')
+parser.add_argument('-f', dest="flat", action='store_true',  help='flatten.')
 args = parser.parse_args()
 
 TEMP_FILE = '.temp'
@@ -43,5 +53,7 @@ drawing = svg2rlg(TEMP_FILE + ".svg")
 renderPM.drawToFile(drawing, TEMP_FILE + ".png", fmt="PNG")
 #cairosvg.svg2png(url=TEMP_FILE + '.svg', write_to=TEMP_FILE + '.png')
 
-
-stripe.picture2Lines(TEMP_FILE + '.png', args.out, args.num)
+if FLATTEN or args.flat :
+    stripe.flatten(TEMP_FILE + '.png', args.out, args.num, white=False)
+else :
+    stripe.picture2Lines(TEMP_FILE + '.png', args.out, args.num, white=False)
