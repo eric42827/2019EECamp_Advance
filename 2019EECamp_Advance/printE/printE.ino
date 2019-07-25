20b80599e0cf74c154f2a6ba98e296ba4c952d0d
tzyLee <b06901145@ntu.edu.tw>
2019-07-25 22:16:46 +0800

    Improve code readability

--- 2019EECamp_Advance/printE/printE.ino
+++ 2019EECamp_Advance/printE/printE.ino
@@ -12,38 +12,34 @@ byte e[] = {254, 146, 146, 146, 146};
 
 float delayTime = 1;
 
-//POV clock cariables
 unsigned long currentMillis, elapsed_loop_counter, previousMillis;
 unsigned long counter_1, current_count;
 
-//Interruption varaibles to count rotation speed
-//We create 4 variables to store the previous value of the input signal (if LOW or HIGH)
-byte last_IN_state;     //Here we store the previous state on digital pin 13
-float one_rot_time = 0; //Here we store the full rotation time
-float time_per_deg = 0; //Here we store the time it takes to make one degree rotation
+byte last_IN_state;
+float one_rot_time = 0; // 轉一圈的時間
+float time_per_deg = 0; // 每一度多少秒
 
 void setup()
 {
-  PCICR |= (1 << PCIE0);   //enable PCMSK0 scan
-  PCMSK0 |= (1 << PCINT4); //Enable pin state interruption on pin D13
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT4); //打開 D13 的 pin state interrupt
 
-  //Output pins register configuration
-  //DDRB |= B00000001;      //8 as output
-
-  delay(3000); // power-up safety delay
+  delay(3000);
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
-  DDRB &= B11101111; //12 input
-
-  //PORTB &= B10111110; //8 to 13 LOW
+  DDRB &= B11101111; // 把 12 設成 input
 }
 
 void draw_a_line(int this_line)
 {
-  for (int i = 0; i < 8; ++i)
-  {
-    leds[i] = this_line & (1 << i)?CRGB:: Red : CRGB::Black;
-  }
+  LEDs[0] = this_line & 0b00000001 ? CRGB::Red : CRGB::Black;
+  LEDs[1] = this_line & 0b00000010 ? CRGB::Red : CRGB::Black;
+  LEDs[2] = this_line & 0b00000100 ? CRGB::Red : CRGB::Black;
+  LEDs[3] = this_line & 0b00001000 ? CRGB::Red : CRGB::Black;
+  LEDs[4] = this_line & 0b00010000 ? CRGB::Red : CRGB::Black;
+  LEDs[5] = this_line & 0b00100000 ? CRGB::Red : CRGB::Black;
+  LEDs[6] = this_line & 0b01000000 ? CRGB::Red : CRGB::Black;
+  LEDs[7] = this_line & 0b10000000 ? CRGB::Red : CRGB::Black;
   FastLED.show();
 }
 
@@ -73,46 +69,31 @@ void loop()
 {
   currentMillis = micros();
   elapsed_loop_counter = currentMillis - previousMillis;
-  delayTime = time_per_deg *2 ; //we want 2 degrees for each line of the letters
+  delayTime = time_per_deg *2; // 每一行兩度
 
-  //This if here is to make sure I'll start printing at 216 deg so the text will be centered.
+  // 從第 216 度開始顯示
   if ((elapsed_loop_counter >= time_per_deg * (216)) && (elapsed_loop_counter < time_per_deg * (217)) && text_ok)
   {
     displayString("e", delayTime);
     text_ok = 0;
   }
-
-/*
-  //This if here is to make sure I'll start printing at 216 deg so the text will be centered.
-  currentMillis = micros();
-  elapsed_loop_counter = currentMillis - previousMillis;
-  delayTime = 2000;
-  //if ((elapsed_loop_counter >= 10 * (216)) && (elapsed_loop_counter < 10 * (217)) && text_ok)
-  //{
-    displayString("ntuee", delayTime);
-    //delayMicroseconds(delayTime*10);
-    //text_ok = 0;
-  //}*/
 }
 
 ISR(PCINT0_vect)
 {
-  //First we take the current count value in micro seconds using the micros() function
-
   current_count = micros();
-  ///////////////////////////////////////Channel 1
-  if (PINB & B00010000)
-  { //We make an AND with the pin state register, We verify if pin 12 is HIGH???
-    if (last_IN_state == 0)
-    {                            //If the last state was 0, then we have a state change...
-      last_IN_state = 1;         //Store the current state into the last state for the next loop
-      counter_1 = current_count; //Set counter_1 to current value.
+  if (PINB & B00010000) // 如果 12 是 HIGH
+  {
+    if (last_IN_state == 0) // 如果 12 從 LOW 變 HIGH
+    {
+      last_IN_state = 1;
+      counter_1 = current_count;
     }
   }
-  else if (last_IN_state == 1)
-  {                                           //If pin 8 is LOW and the last state was HIGH then we have a state change
-    last_IN_state = 0;                        //Store the current state into the last state for the next loop
-    one_rot_time = current_count - counter_1; //We make the time difference. Channel 1 is current_time - timer_1.
+  else if (last_IN_state == 1) // 如果 12 從 HIGH 變 LOW
+  {
+    last_IN_state = 0;
+    one_rot_time = current_count - counter_1;
     time_per_deg = one_rot_time / 360.0;
     previousMillis = micros();
     text_ok = 1;
